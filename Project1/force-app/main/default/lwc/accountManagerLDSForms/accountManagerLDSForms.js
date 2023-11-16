8cfb11ad584838c605b37db0d18218b284845278
Nandan Hegde <hegdenandan013@gmail.com>
2023-11-16 14:12:14 +0530

    new changes

---
+++ Project1/force-app/main/default/lwc/accountManagerLDSForms/accountManagerLDSForms.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class AccountManagerLDSForms extends LightningElement {
+    recordId;
+
+    successHandler(event)
+    {
+          this.recordId = event.detail.id;
+    }
+
+}
